d11f4a03116499fb01e46a4edd9caa64f220b87a
Rahul Sharma <08rahul.sharma01@gmail.com>
2020-07-30 15:52:50 +0530

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,72 @@
+function changeDarkBlue(){
+    document.getElementById('body').style.background = "#192A56";
+    document.getElementById('exp-container').style.color = "white";
+    document.getElementById('project-con').style.color = "white";
+    document.getElementById('skills-con').style.color = "white";
+    document.getElementById('education-con').style.color = "white";
+    document.getElementById('contact-con').style.color = "white";
+    document.getElementById('profile-con').style.color = "white";
+    document.getElementById('prof-img').style.background = "white";
+    document.getElementById('hr').style.border= "1px solid white";
+    document.getElementById('hr1').style.border= "1px solid white";
+    document.getElementById('hr2').style.border= "1px solid white";
+    document.getElementById('dates').style.color="yellow";
+    document.getElementById('dates2').style.color="yellow";
+    document.getElementById('dates3').style.color="yellow";
+    document.getElementById('dates4').style.color="yellow";
+    document.getElementById('dates5').style.color="yellow";
+    document.getElementById('dates6').style.color="yellow";
+}
+
+function changeGrey(){
+    document.getElementById('body').style.background = "#2F363F";
+    document.getElementById('exp-container').style.color = "white";
+    document.getElementById('project-con').style.color = "white";
+    document.getElementById('skills-con').style.color = "white";
+    document.getElementById('education-con').style.color = "white";
+    document.getElementById('contact-con').style.color = "white";
+    document.getElementById('profile-con').style.color = "white";
+    document.getElementById('prof-img').style.background = "white";
+    document.getElementById('hr').style.border= "1px solid white";
+    document.getElementById('hr1').style.border= "1px solid white";
+    document.getElementById('hr2').style.border= "1px solid white";
+    document.getElementById('dates').style.color="yellow";
+    document.getElementById('dates2').style.color="yellow";
+    document.getElementById('dates3').style.color="yellow";
+    document.getElementById('dates4').style.color="yellow";
+    document.getElementById('dates5').style.color="yellow";
+    document.getElementById('dates6').style.color="yellow";
+}
+
+function changePeach(){
+    document.getElementById('body').style.background = "#EA7773";
+    document.getElementById('exp-container').style.color = "white";
+    document.getElementById('project-con').style.color = "white";
+    document.getElementById('skills-con').style.color = "white";
+    document.getElementById('education-con').style.color = "white";
+    document.getElementById('contact-con').style.color = "white";
+    document.getElementById('profile-con').style.color = "white";
+    document.getElementById('prof-img').style.background = "white";
+    document.getElementById('hr').style.border= "1px solid white";
+    document.getElementById('hr1').style.border= "1px solid white";
+    document.getElementById('hr2').style.border= "1px solid white";
+    document.getElementById('dates').style.color="yellow";
+    document.getElementById('dates2').style.color="yellow";
+    document.getElementById('dates3').style.color="yellow";
+    document.getElementById('dates4').style.color="yellow";
+    document.getElementById('dates5').style.color="yellow";
+    document.getElementById('dates6').style.color="yellow";
+}
+
+function changeGreen(){
+    document.getElementById('body').style.background = "#53E0BC";
+    document.getElementById('hr').style.border= "1px solid white";
+    document.getElementById('hr1').style.border= "1px solid white";
+    document.getElementById('hr2').style.border= "1px solid white";
+    document.getElementById('dates').style.color="#2F363F";
+    document.getElementById('dates2').style.color="#2F363F";
+    document.getElementById('dates3').style.color="#2F363F";
+    document.getElementById('dates4').style.color="#2F363F";
+    document.getElementById('dates5').style.color="#2F363F";
+    document.getElementById('dates6').style.color="#2F363F";
+}
